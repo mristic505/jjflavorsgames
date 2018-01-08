f82862f7ca2991934e0f5840913ceecd588e9be4
Rodolfo Reynoso <rodolfo.reynoso@icloud.com>
2018-01-07 19:40:37 -0500

    Add links to Spin and Get Coupon Btns

--- laugh-factory.php
+++ laugh-factory.php
@@ -26,8 +26,8 @@
     <p id="fruityFact"></p>
     <div id="slide3-BtnGroup">
       <button id="jokesAgainBtn" onclick="plusSlides(-1)">SEE JOKES AGAIN<img class="rightArrow" src="img/rightArrow.png"></button>
-      <button id="spinBtn">SPIN<img class="rightArrow" src="img/rightArrow.png"></button>
-      <button id="getCoupBtn">GET COUPON</button>
+      <button id="spinBtn" onclick="location.href='?page=spin'; ">SPIN<img class="rightArrow" src="img/rightArrow.png"></button>
+      <button id="getCoupBtn" onclick="location.href='?page=coupon';">GET COUPON</button>
     </div>  
   </div>
   
