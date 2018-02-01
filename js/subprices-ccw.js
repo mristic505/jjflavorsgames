b9a557ce0fec295b0aeb971b4b979b3eb2708db7
Mateja Ristic <mristic@brandconnections.com>
2018-02-01 01:09:26 -0500

    uncomment shuffle()

--- js/subprices-ccw.js
+++ js/subprices-ccw.js
@@ -370,7 +370,7 @@ jQuery(document).ready(function($) {
         return $(shuffled);
     };
 
-    // $('.clickme').shuffle();
+    $('.clickme').shuffle();
 
     $(".clickme").flip({
         axis: 'x',
